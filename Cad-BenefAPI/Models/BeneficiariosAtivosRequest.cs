921c782f91b62eb023bf39ce004d61dc8db8a4aa
kleitonrds <kleitonrds@msn.com>
2024-12-03 14:03:00 -0300

    Adicionando validação no formato da data AAA-MM-DD

---
+++ Cad-BenefAPI/Models/BeneficiariosAtivosRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cad_BenefAPI.Models
+{
+    public class BeneficiariosAtivosRequest
+    {
+        [Required]
+        public string? CodigoUnimed { get; set; }
+
+        [Required(ErrorMessage = "A data de início é obrigatória.")]
+        [RegularExpression(@"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$",ErrorMessage = "A data início deve estar no formato AAAA-MM-DD.")]
+        public string? DataInicio { get; set; }
+
+        [Required(ErrorMessage = "A data fim é obrigatória.")]
+        [RegularExpression(@"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$", ErrorMessage = "A data fim deve estar no formato AAAA-MM-DD.")]
+        public string? DataFim { get; set; }
+    }
+}
